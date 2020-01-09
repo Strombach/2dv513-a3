db9b649e43e4a22f42ce5567da5ce4c986774051
Strombach <fredde.stromback@gmail.com>
2020-01-09 17:58:08 +0100

    Tests to query database

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 'use strict'
 
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const expressPort = 3000
+
+let connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "imduh"
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM movies', (err, rows, fields) => {
+	if (err) throw err
+
+	console.log(rows)
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
