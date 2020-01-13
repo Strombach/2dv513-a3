bf6aa4a16469017592c0e2ea08a694b6f2dfb3a6
Strombach <fredde.stromback@gmail.com>
2020-01-13 17:00:13 +0100

    Removes code

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -15,24 +15,6 @@ homeController.index = (req, res, next) => {
 		res.render('home/index', { locals })
 	})
 }
-// loginController.postLogin = async (req, res, next) => {
-
-// 	db.query('SELECT * FROM users WHERE username = ?', req.body.username, async (err, rows, fields) => {
-// 		if (err) throw err
-
-// 		if (rows.length > 0) {
-// 			if (checkPassword(rows[0], req.body.password)) {
-// 				req.session.loggedIn = rows[0].userID
-// 				fetchMyMovies(rows[0].userID)
-// 				res.redirect('/home')
-// 			} else {
-// 				res.send('Wrong Password')
-// 			}
-// 		} else {
-// 			res.send('No user found')
-// 		}
-// 	})
-// }
 
 homeController.checkLoginStatus = (req, res, next) => {
 	if (!req.session.loggedIn) {
