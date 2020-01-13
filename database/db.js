de0474b68df48f6d787ec6b4d660904d0cd26b5e
filipmyllari <fm222hp@student.lnu.se>
2020-01-13 14:46:22 +0100

    port variable added

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
 
 let connection = mysql.createConnection({
-	host: process.env.DB_URL,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_DB
+  host: process.env.DB_URL,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DB
 })
 
 connection.connect()
